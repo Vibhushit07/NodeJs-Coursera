6074154e581b0b14cada7d42445907b67a99e5f5
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-07-07 19:55:17 +0530

    Express REST API with MongoDB and Mongoose Part 1

--- conFusionServer/routes/leaderRouter.js
+++ conFusionServer/routes/leaderRouter.js
@@ -5,22 +5,6 @@ const leaderRouter = express.Router();
 
 leaderRouter.use(bodyParser.json());
 
-leaderRouter.route('/:leaderId')
-.get((req, res, next) => {
-    res.end('Will send details of the leader: ' + req.params.leaderId + ' to you!');
-})
-.post((req, res, next) => {
-    res.statusCode = 403;
-    res.end('Post operation not supported on /leaders/' + req.params.leaderId);
-})
-.put((req, res, next) => {
-    res.write('Updating the leader: ' + req.params.leaderId + '\n');
-    res.end('Will update the leader: ' + req.body.name + ' with details: ' + req.body.description);
-})
-.delete((req, res, next) => {
-    res.end('Deleting leader: ' + req.params.leaderId);
-});
-
 leaderRouter.route('/')
 .all((req, res, next) => {
     res.statusCode = 200;
@@ -39,6 +23,22 @@ leaderRouter.route('/')
 })
 .delete((req, res, next) => {
     res.end('Deleting all the leaders!');
+});
+
+leaderRouter.route('/:leaderId')
+.get((req, res, next) => {
+    res.end('Will send details of the leader: ' + req.params.leaderId + ' to you!');
+})
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('Post operation not supported on /leaders/' + req.params.leaderId);
+})
+.put((req, res, next) => {
+    res.write('Updating the leader: ' + req.params.leaderId + '\n');
+    res.end('Will update the leader: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.delete((req, res, next) => {
+    res.end('Deleting leader: ' + req.params.leaderId);
 });
 
 module.exports = leaderRouter;
--- conFusionServer/routes/promoRouter.js
+++ conFusionServer/routes/promoRouter.js
@@ -5,22 +5,6 @@ const promoRouter = express.Router();
 
 promoRouter.use(bodyParser.json());
 
-promoRouter.route('/:promoId')
-.get((req, res, next) => {
-    res.end('Will send details of the promotion: ' + req.params.promoId + ' to you!');
-})
-.post((req, res, next) => {
-    res.statusCode = 403;
-    res.end('Post operation not supported on /promotions/' + req.params.promoId);
-})
-.put((req, res, next) => {
-    res.write('Updating the promotion: ' + req.params.promoId + '\n');
-    res.end('Will update the promotion: ' + req.body.name + ' with details: ' + req.body.description);
-})
-.delete((req, res, next) => {
-    res.end('Deleting promotion: ' + req.params.promoId);
-});
-
 promoRouter.route('/')
 .all((req, res, next) => {
     res.statusCode = 200;
@@ -39,6 +23,22 @@ promoRouter.route('/')
 })
 .delete((req, res, next) => {
     res.end('Deleting all the promotions!');
+});
+
+promoRouter.route('/:promoId')
+.get((req, res, next) => {
+    res.end('Will send details of the promotion: ' + req.params.promoId + ' to you!');
+})
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('Post operation not supported on /promotions/' + req.params.promoId);
+})
+.put((req, res, next) => {
+    res.write('Updating the promotion: ' + req.params.promoId + '\n');
+    res.end('Will update the promotion: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.delete((req, res, next) => {
+    res.end('Deleting promotion: ' + req.params.promoId);
 });
 
 module.exports = promoRouter;
