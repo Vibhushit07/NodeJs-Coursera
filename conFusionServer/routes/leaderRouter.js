e6076bff6fb3d340c0abf6f7896e5a049541d169
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-07-08 17:44:07 +0530

    Assignment 2 Task 2-a

--- conFusionServer/routes/leaderRouter.js
+++ conFusionServer/routes/leaderRouter.js
@@ -6,11 +6,6 @@ const leaderRouter = express.Router();
 leaderRouter.use(bodyParser.json());
 
 leaderRouter.route('/')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
 .get((req, res, next) => {
     res.end('Will send all the leaders to you!');
 })
