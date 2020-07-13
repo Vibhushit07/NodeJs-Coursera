0d5e598d4fff2b96149542a26ca6a5ae2d6bfaf9
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-07-14 00:49:42 +0530

    Express Session Part 2

--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -1,9 +1,40 @@
 var express = require('express');
+const bodyParser = require('body-parser');
+var User = require('../models/user');
+const e = require('express');
+
 var router = express.Router();
+router.use(bodyParser.json());
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/signup', function(req, res, next){
+
+  User.findOne({ username: req.body.username })
+  .then((user) => {
+    if(user != null) {
+      var err = new Error('User ' + req.body.username + ' already exist');
+      err.status = 403;
+      next(err);
+    
+    } else {
+      
+      User.create({
+        username: req.body.username,
+        password: req.body.password
+      });
+    }
+  })
+  .then(user => {
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({ status: 'Registration Successful', user: user });
+  }, err => next(err))
+  .catch(err => next(err));
 });
 
 module.exports = router;
