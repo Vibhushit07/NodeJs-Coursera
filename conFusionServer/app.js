72ba8ac5c9c6fa099890d5f3427a98f54e496f84
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-08-05 03:36:21 +0530

    HTTPS

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -20,15 +20,22 @@ const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
 
 const url = config.mongoUrl;
-const connect = mongoose.connect(url, {
-  useMongoClient: true
-});
+const connect = mongoose.connect(url);
 
 connect.then((db) => {
   console.log('Connected correctly to server');
 }, (err) => { console.log(err); });
 
 var app = express();
+
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+
+  } else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
